d42c51a2311c4aec60236822e231e92136b11049
Dana Wang <wangxia_2008@163.com>
2018-06-21 14:45:38 +0800

    add config server sample

---
+++ config-server/src/main/java/com/springcloud/config/ConfigController.java
@@ -0,0 +1,17 @@
+package com.springcloud.config;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangxia on 2018/6/21.
+ */
+@RestController
+public class ConfigController {
+
+    @RequestMapping("/config")
+    public String config() {
+
+        return "hello, this is a config server."
+    }
+}
---
+++ config-server/src/main/java/com/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by wangxia on 2018/6/21.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
