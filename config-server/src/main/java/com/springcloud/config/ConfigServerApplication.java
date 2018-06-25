022baaf75061eb51329db278ac7be63d303767c1
Dana Wang <wangxia_2008@163.com>
2018-06-25 18:16:11 +0800

    Updated for config server using git

--- config-server/src/main/java/com/springcloud/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/springcloud/config/ConfigServerApplication.java
@@ -2,6 +2,8 @@ package com.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -12,7 +14,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+//        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
